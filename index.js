9a957fdcc0925d2b754e87d6a46c520c78f6d05d
VusalMammadli <130961501+VusalMammadli@users.noreply.github.com>
2025-04-02 13:51:09 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const btn1 = document.getElementById("btn1")
 const btn2 = document.getElementById("btn2")
 const btn3 = document.getElementById("btn3")
 let i=0;
-let a=0;
 
 
 btn1.addEventListener("click",function(){
-    spn.innerText=i--;
+    i--;
+    spn.innerText=i;
 })
 btn2.addEventListener("click", function(){
-    spn.innerText=0;
+    i = 0
+    spn.innerText=i;
     
 })
 btn3.addEventListener("click",function(){
-    spn.innerText=a++;
+    i++;
+    spn.innerText=i;
 })
